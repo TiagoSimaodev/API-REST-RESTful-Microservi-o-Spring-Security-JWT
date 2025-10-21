75e4613536d2dda6a1bb877842d434d1f6e92d26
tiago simao <tiagosimaorodri123@gmail.com>
2025-10-21 01:00:04 -0300

    criado a query para pesquisar por nome.

--- src/main/java/projeto/api/rest/repository/UsuarioRepository.java
+++ src/main/java/projeto/api/rest/repository/UsuarioRepository.java
@@ -2,6 +2,8 @@ package projeto.api.rest.repository;
 
 
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
@@ -20,4 +22,9 @@ public interface UsuarioRepository extends CrudRepository<Usuario, Long> {
 	@Modifying
 	@Query(nativeQuery = true, value = "update usuario set token = ?1 where login = ?2")
 	void atualizaTokenUser(String token, String login);
+	
+
+	@Query("select u from Usuario u where u.nome like %?1%")
+	List<Usuario> findUserByNome(String nome);
+	
 }
